1b325e8544c07d62e2c452edfb749bc341127561
JocaCavalcanti <joaomcavalcanti.jmcm@gmail.com>
2023-08-08 15:43:37 -0300

    :bug: fix: Adding links and fixing bugs in the brench

--- src/assets/components/Contact/Contact.jsx
+++ src/assets/components/Contact/Contact.jsx
@@ -11,45 +11,45 @@ import whatsapp from '../../images/whatsapp.svg'
 const Contact = () => {
   return (
     <div id="contactDetails">
-        <div className="contactDetailsLogo">
-          <i>
-            <img src={logo} alt="" className="contactLogo" />
-          </i>
-        </div>
-        <div className="contactDetailsContent">
-          <h3 className="contactDetailsTitle">Detalhes do contato</h3>
-          <div className="contactIcons">
-            <div className="contactTwoIconsBox">
-              <Icons
-                imgIcon={phone}
-                title="Ligue para mim"
-                desc="+55 (81) 9663-5917"
-              />
-
-              <Icons 
-              imgIcon={email} 
-              title="Email" 
-              desc="atvmt4.0@gmail.com" 
-              />
-            </div>
-            <div className="contactTwoIconsBox">
-              <Icons
-                imgIcon={location}
-                title="Localização"
-                desc="Olinda-Pe"
-              />
-
-              <Icons 
-              imgIcon={user} 
-              title="Redes Sociais" 
-              descImg={instagram}
-              descImg2={whatsapp}
-              />
-            </div>
-
-          </div>
-        </div> 
+      <div className="contactDetailsLogo">
+        <i>
+          <img src={logo} alt="" className="contactLogo" />
+        </i>
       </div>
+      <div className="contactDetailsContent">
+        <h3 className="contactDetailsTitle">Detalhes do contato</h3>
+        <div className="contactIcons">
+          <div className="contactTwoIconsBox">
+            <Icons
+              imgIcon={phone}
+              title="Ligue para mim"
+              desc="+55 (81) 9663-5917"
+            />
+
+            <Icons
+              imgIcon={email}
+              title="Email"
+              desc="atvmt4.0@gmail.com"
+            />
+          </div>
+          <div className="contactTwoIconsBox">
+            <Icons
+              imgIcon={location}
+              title="Localização"
+              desc="Olinda-Pe"
+            />
+
+            <Icons
+              imgIcon={user}
+              title="Redes Sociais"
+              descImg={instagram}
+              link="https://instagram.com/ativamenth?igshid=MzRlODBiNWFlZA=="
+            />
+          </div>
+
+        </div>
+      </div>
+    </div>
   );
 };
 export default Contact;
